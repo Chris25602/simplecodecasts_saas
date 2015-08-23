248b74ccd2a15d2bea11ec14c0f5481ce27c70cb
Chris Richardson <crichardson25602@gmail.com>
2015-08-23 00:15:49 +0000

    remove stripe integration errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,10 @@ class ApplicationController < ActionController::Base
         
     end
     
-    before_filter :configure_permitted_parameters, if: :devise_controller?
-    
-    protected
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :marsId, :email, :password, :password_confirmation) }
-    end
+    #before_filter :configure_permitted_parameters, if: :devise_controller?
+    #
+    #protected
+    #def configure_permitted_parameters
+    #  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :marsId, :email, :password, :password_confirmation) }
+    #end
 end
