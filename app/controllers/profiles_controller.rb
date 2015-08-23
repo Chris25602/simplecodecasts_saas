27e9145ab71021aadbf92bca07576d903de9903b
Chris Richardson <crichardson25602@gmail.com>
2015-08-23 15:42:05 +0000

    more profile work, and fixed marsId on Pro login

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,12 +1,7 @@
 class ProfilesController < ApplicationController
-	
-	def new
-	#form for each user to fill out their own profile	
-	@user = User.find( params[:user_id])
-	@profile = @user.build_profile
-	#
-	#
-	end
-	
-	
+  def new
+    # form where a user can fill out their own profile.
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile
+  end	
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,8 +7,6 @@ def create
 			
 			resource.plan_id = params[:plan]
 			if resource.plan_id == 2
-				@marsId = params[:marsId]
-				resource.marsId = @marsId
 				#resource.save_with_payemnt if stripe integration is added
 				resource.save_with_marsId
 			else
