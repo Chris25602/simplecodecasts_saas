efb145ff5016955db7aa1f43a49dc97bcd64acdf
Chris Richardson <crichardson25602@gmail.com>
2015-08-27 01:31:44 +0000

    more reporting scaffolding

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,7 @@
 class ReportsController < ApplicationController
 before_action :authenticate_user!
 def create 
-	@report = Report.new(report_col: 1)
+	
 end
 def new
 	@user = User.find( params[:user_id] )
